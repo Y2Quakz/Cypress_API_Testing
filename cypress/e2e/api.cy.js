0f3af2a02e1315450290236fdc33c8897f8a12d5
El Moussaoui <khalid.el-moussaoui@sogeti.com>
2024-10-21 04:47:45 +0200

    Validate Headers

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,5 +1,24 @@
-describe('template spec', () => {
+
+
+describe('Learn REST API Testing with Cypress', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.request('/users/2').then((response) => {
+      cy.log(JSON.stringify(response.body.data.email))
+      cy.log(JSON.stringify(response.headers.headers))
+
   })
+})
+
+  it('API Tests - Validate Headers', () => {
+    cy.request('/users/2').as('user') 
+    cy.get('@user')
+      .its('headers')
+      .its('content-type')
+      .should('include', 'application/json')
+
+    cy.get('@user').its('headers').its('connection').should('include', 'keep-alive')
+
+  })
+
+
 })
